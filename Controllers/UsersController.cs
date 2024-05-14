dae797518714ccdb2dc5d86c3b23b7cb42e17527
Murillo Oliveira <murillo1047@gmail.com>
2024-05-13 21:15:52 -0300

    feat: sign in and login users

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using UserProductAPI.Data.DTOS;
+using UserProductAPI.Services;
+
+namespace UserProductAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsersController: ControllerBase
+    {
+        private UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> CreateUser(CreateUserDTO userDTO)
+        {
+            await _userService.CreateUser(userDTO);
+            return Ok("Usuário criado com sucesso");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(LoginUserDTO userDTO)
+        {
+            var token = await _userService.LoginUser(userDTO);
+            return Ok(token);
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using UserProductAPI.Data.DTOS;
+using UserProductAPI.Data.Models;
+
+namespace UserProductAPI.Services
+{
+    public class UserService
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly TokenService _tokenService;
+
+        public UserService(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager, TokenService tokenService)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        public async Task CreateUser(CreateUserDTO userDTO)
+        {
+            if (!userDTO.ValidateRole())
+            {
+                throw new ApplicationException("Role deve ser 'admin' ou 'user'.");
+            }
+
+            User user = _mapper.Map<User>(userDTO);
+
+            IdentityResult result = await _userManager.CreateAsync(user, userDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                throw new ApplicationException("Falha ao cadastrar usuário!");
+            }
+        }
+
+        public async Task<string> LoginUser(LoginUserDTO userDTO)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userDTO.Username, userDTO.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                throw new ApplicationException("Usuário não autenticado");
+            }
+
+            var user = _signInManager.UserManager.Users.FirstOrDefault(user => user.NormalizedUserName == userDTO.Username.ToUpper());
+
+            var token = _tokenService.GenerateToken(user);
+
+            return token;
+        }
+    }
+}
